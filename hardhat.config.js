9694c13516fdaa6c175daef3b518f387bc763e2f
Deepak <deepakkomma@gmail.com>
2022-12-03 18:05:26 +0530

    Added hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+require("dotenv").config();
+const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "mumbai",
+  networks: {
+    mumbai: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
